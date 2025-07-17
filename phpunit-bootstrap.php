d38bc604fb4ed38f77b4183d261f425523192937
Chris Reynolds <chris@jazzsequence.com>
2025-07-17 16:32:28 -0600

    remove phpcs stuff

--- phpunit-bootstrap.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Set the PHPCS standard directory for the test framework.
-putenv('PHP_CODESNIFFER_STANDARD_DIRS=' . __DIR__);
-
-// Now include the PHPCS test bootstrap.
-require_once __DIR__ . '/vendor/squizlabs/php_codesniffer/tests/bootstrap.php';

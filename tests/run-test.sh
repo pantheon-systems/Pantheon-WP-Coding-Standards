7f7007047a09e7ba8b34a3d8fa72585cf7cad951
Chris Reynolds <chris@jazzsequence.com>
2025-07-18 13:05:29 -0600

    include severity in the tests

--- tests/run-test.sh
+++ tests/run-test.sh
@@ -18,19 +18,18 @@ fi
 TEST_NAME=$(sed -n '3s/ \* //p' "$TEST_FILE")
 
 echo ""
-echo "Running test: $TEST_NAME with ruleset: $RULESET"
+echo "Running test: ${TEST_NAME} with ruleset: $RULESET"
 echo "$TEST_FILE"
 
 # Use --basepath=. to ensure relative paths in the report.
 JSON_REPORT=$(phpcs --standard="./$RULESET/ruleset.xml" --report=json --basepath=. "$TEST_FILE" 2>/dev/null | sed -n '/{/,$p')
 
 # Debug
-# echo "JSON Report:"
-# echo "$JSON_REPORT"
+# echo "JSON Report: ${JSON_REPORT}"
 
-# Extract actual and expected errors.
-ACTUAL_OUTPUT=$(echo "$JSON_REPORT" | jq -r '.files."'"$TEST_FILE"'".messages[]?.source' | sort)
-EXPECTED_OUTPUT=$( (sed -n "s/.*@expectedError\[$RULESET\] //p" "$TEST_FILE"; sed -n "s/.*@expectedWarning\[$RULESET\] //p" "$TEST_FILE") | sort)
+# Extract actual and expected errors, including severity.
+ACTUAL_OUTPUT=$(echo "$JSON_REPORT" | jq -r '.files."'"$TEST_FILE"'".messages[] | "[\(.type | ascii_upcase)] \(.source)"' | sort)
+EXPECTED_OUTPUT=$( (sed -n "s/.*@expectedError\[$RULESET\] /\[ERROR\] /p" "$TEST_FILE"; sed -n "s/.*@expectedWarning\[$RULESET\] /\[WARNING\] /p" "$TEST_FILE") | sort)
 
 echo "--------------------------------------------------"
 echo "EXPECTED ISSUES:"

f9229fa3a3bf52b89db20147116218154fb45497
Chris Reynolds <chris@jazzsequence.com>
2025-07-18 12:32:23 -0600

    break the reusable single line comment code into a static method

--- Pantheon-WP-Minimum/Sniffs/Commenting/DisallowMultilineSlashCommentSniff.php
+++ Pantheon-WP-Minimum/Sniffs/Commenting/DisallowMultilineSlashCommentSniff.php
@@ -25,34 +25,59 @@ class DisallowMultilineSlashCommentSniff implements Sniff {
 	}
 
 	/**
-		* Processes this test, when one of its tokens is encountered.
-		*
-		* @param \PHP_CodeSniffer\Files\File $phpcsFile The file being scanned.
-		* @param int                         $stackPtr  The position of the current token in the
-		*                                               stack passed in $tokens.
-		*
-		* @return void
-		*/
+	 * Processes this test, when one of its tokens is encountered.
+	 *
+	 * @param \PHP_CodeSniffer\Files\File $phpcsFile The file being scanned.
+	 * @param int                         $stackPtr  The position of the current token in the
+	 *                                               stack passed in $tokens.
+	 *
+	 * @return void
+	 */
 	public function process( File $phpcsFile, $stackPtr ) {
+		self::getConsecutiveSingleLineComments( $phpcsFile, $stackPtr );
+	}
+
+	/**
+	 * Checks for consecutive single-line comments and reports them.
+	 *
+	 * @param File $phpcsFile The file being scanned.
+	 * @param int  $stackPtr  The position of the current token in the stack.
+	 *
+	 * @return void
+	 */
+	public static function getConsecutiveSingleLineComments( File $phpcsFile, $stackPtr ) {
 		$tokens = $phpcsFile->getTokens();
+		$token  = $tokens[ $stackPtr ];
 
 		// We are only interested in single-line comments.
-		if ( '//' !== substr( $tokens[ $stackPtr ]['content'], 0, 2 ) ) {
+		if ( '//' !== substr( $token['content'], 0, 2 ) ) {
 			return;
 		}
 
-		$nextComment = $phpcsFile->findNext( T_COMMENT, $stackPtr + 1 );
-		if ( false === $nextComment ) {
-			return;
-		}
-
-		// Check if the next comment is on the next line.
-		if ( $tokens[ $nextComment ]['line'] === $tokens[ $stackPtr ]['line'] + 1 ) {
-			// Check if the next comment is also a single-line comment.
-			if ( '//' === substr( $tokens[ $nextComment ]['content'], 0, 2 ) ) {
-				$error = 'Multiline comments should use /* ... */ syntax, not multiple // comments.';
-				$phpcsFile->addWarning( $error, $stackPtr, 'Found' );
+		// Check the previous line to see if we are already in a block.
+		$prevLine = $token['line'] - 1;
+		$prevComment = $phpcsFile->findPrevious( T_COMMENT, $stackPtr - 1, null, false, null, true );
+		if ( false !== $prevComment && $tokens[$prevComment]['line'] === $prevLine ) {
+			if ( '//' === substr( $tokens[$prevComment]['content'], 0, 2 ) ) {
+				return; // This is not the start of the block.
 			}
+		}
+
+		// This is the start of a block. Now, count how many lines it spans.
+		$lineCount = 1;
+		$nextComment = $stackPtr;
+		while ( true ) {
+			$nextComment = $phpcsFile->findNext( T_COMMENT, $nextComment + 1, null, false, null, true );
+			if ( false === $nextComment || $tokens[$nextComment]['line'] !== ( $token['line'] + $lineCount ) ) {
+				break; // The block has ended.
+			}
+			$lineCount++;
+		}
+
+		if ( $lineCount > 1 ) {
+			$message = 'A block of %s consecutive single-line comments was found starting on this line. Please use a /* ... */ block instead.';
+			$data    = [ $lineCount ];
+			$phpcsFile->addWarning( $message, $stackPtr, 'Found', $data );
 		}
 	}
 }
